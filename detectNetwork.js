b1db0e678043bf7155de7727de07ab2e234e9f85
lhuynh27 <leannehuynh27@gmail.com>
2018-05-13 17:31:37 -0700

    Coded in prefix and lengths for Diner's club and AE

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,7 +11,13 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-
+  var detectNetwork = function(cardNumber) {
+  if ((cardNumber.startsWith('38') || cardNumber.startsWith('39')) && cardNumber.length === 14){
+    return "Diner's Club";
+  }
+  if ((cardNumber.startsWith('34') || cardNumber.startsWith('37')) && cardNumber.length === 15){
+      return "American Express";
+  }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
 
