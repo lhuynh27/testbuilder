b20b232d2256bd6f00be9d59c22803a73b1a8599
lhuynh27 <leannehuynh27@gmail.com>
2018-05-14 00:42:50 -0700

    Added Discover and Maestro Test

--- detectNetwork.js
+++ detectNetwork.js
@@ -23,6 +23,19 @@ var detectNetwork = function(cardNumber) {
   if ((cardNumber.startsWith('51') || cardNumber.startsWith('52') || cardNumber.startsWith('53') || cardNumber.startsWith('54') || cardNumber.startsWith('55')) && cardNumber.length === 16){
     return "MasterCard";
   } 
+  if ((cardNumber.startsWith('6011') || 
+       cardNumber.startsWith('644') || 
+       cardNumber.startsWith('645') || 
+       cardNumber.startsWith('646') ||
+       cardNumber.startsWith('647') ||
+       cardNumber.startsWith('648') ||
+       cardNumber.startsWith('649') ||
+       cardNumber.startsWith('65')) && (cardNumber.length === 16 || cardNumber.length === 19)){
+    return "Discover";
+  } 
+  if ((cardNumber.startsWith('5018') || cardNumber.startsWith('5020') || cardNumber.startsWith('5038') || cardNumber.startsWith('6304')) && (19 >= cardNumber.length >= 12)){
+    return "Maestro";
+  }     
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
 
